e7ba9d82560ba6f6925f926271d86b4fad464196
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-10-18 13:42:21 -0300

    API de Identidade

---
+++ src/web/MSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSE.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
